7143b3441655279c65d57ef58d7262f84a89c42d
Nnekalyn <nneka.e.lyn@gmail.com>
2026-07-03 17:07:46 -0400

    feature/directory sweeper function

--- scripts/log_parser.py
+++ scripts/log_parser.py
@@ -1,6 +1,35 @@
-def clear_the_gatekeeper():
-    test_message = ("Good Job, Neeks! Pipeline verification successful")
-    print(test_message)
+#Log Parser
 
-if __name__ == "__main__":
-    clear_the_gatekeeper()
+
+import os
+
+# test directory
+path = "/Users/neeks/PycharmProjects/LogParserScript/tests"
+
+def directory_sweeper(path):
+    '''Inputs: directory path to unorganized log files
+       Outputs: yields file paths for .log/.txt files
+       Exceptions: UnicodeError, PermissionError, FileNotFoundError '''
+        with os.scandir(path) as entries:
+            for entry in entries:
+                if entry.is_file():
+                    if not entry.is_symlink():
+                        file_path = entry.path
+                        file_path = file_path.lower()
+                        if file_path.endswith(('.txt', '.log')):
+                            yield file_path
+                        elif file_path.count('.') < 1:
+                            try:
+                                with open(file_path, "r") as file:
+                                    try:
+                                        file.read(1)
+                                        yield file_path
+                                    except UnicodeDecodeError:
+                                        continue
+                            except (PermissionError, FileNotFoundError):
+                                yield file_path
+
+
+print(list(directory_sweeper(path)))
+
+

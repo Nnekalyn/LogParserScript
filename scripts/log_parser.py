fbddb6a1c3bd647b4dda1647512cb1d4fbe9b56e
Nnekalyn <nneka.e.lyn@gmail.com>
2026-07-09 17:52:56 -0400

    Remove print statements & replace w/ storage actions

--- scripts/log_parser.py
+++ scripts/log_parser.py
@@ -2,6 +2,9 @@
 
 import os
 import re
+import json
+from collections import Counter
+import datetime
 
 
 def directory_sweeper(path):
@@ -34,31 +37,51 @@ def directory_sweeper(path):
 
 
 def parsing_gate(file_path):
+    local_counts = Counter()
+    local_malformed = []
     try:
         with open(file_path, 'r') as file:
             for line_num, line in enumerate(file, 1):
                 try:
-                    log = re.search(r"^(\d{4}-\d{2}-\d{2})\s+\[(\w+)\]\s+(.*)$", line)
+                    log = re.search(r"^"
+                                    r"(\d{4}-\d{2}-\d{2})"
+                                    r"\s+\[(\w+)]\s+(.*)"
+                                    r"$", line)
                     if log is None:
-                        print(f"File {file_path} | Line {line_num}: MALFORMED LOG")
+                        local_malformed.append({"file": file_path, "line": line_num})
                         continue
                     date, log_level, message = log.group(1), log.group(2), log.group(3)
-                    print(f"    -> SUCCESS: found {date} [{log_level}] message: {message}")
+                    local_counts[log_level] += 1
                 except Exception as e:
                     print(f"Unexpected error parsing line {line_num}: {e}")
                     continue
+        return {
+            "status": "success",
+            "log_level": local_counts,
+            "malformed": local_malformed
+        }
     except PermissionError:
-        print(f"Missing Permissions. File path:{file_path}")
+        return {"status": "failed", "log_level": Counter(), "malformed": []}
     except FileNotFoundError:
-        print(f"File Not Found. File path:{file_path}")
+        return {"status": "failed", "log_level": Counter(), "malformed": []}
 
 if __name__ == "__main__":
 
     # test directory
     path = "/Users/neeks/PycharmProjects/LogParserScript/tests"
 
+    total_files_processed = 0
+    global_log_counts = Counter()
+    global_malformed_records = []
+
+    print("---Ingestion Pipeline Active---\n")
+
     for path_to_parse in directory_sweeper(path):
-        print(f"Processing next available resource: {path_to_parse}")
-        parsing_gate(path_to_parse)
+        file_metrics = parsing_gate(path_to_parse)
+
+        if file_metrics["status"] == "success":
+            total_files_processed += 1
+        global_log_counts.update(file_metrics["log_level"])
+        global_malformed_records.extend(file_metrics["malformed"])
 
     print("\n--- Pipeline Complete ---")
